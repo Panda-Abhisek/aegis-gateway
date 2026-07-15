defe5526f842a22873856bfad8e7596b4e2f546c
Panda-Abhisek <abhisekpanda114@gmail.com>
2026-07-15 22:04:13 +0530

    Initial Spring Boot project setup
    - Java 21, Spring Boot 3.4.5, Spring Cloud Gateway 2024.0.1
    - Maven project with spring-boot-starter-gateway dependency
    - AegisGatewayApplication entry point
    - ContextLoads test passing

---
+++ src/test/java/com/panda/aegis/AegisGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panda.aegis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AegisGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
